412cfba40ba50b474b8fab947cbc1324cd87c77b
Chema <mrjark7@gmail.com>
2025-10-22 17:37:11 +0200

    created riddle

---
+++ src/riddles/riddle.ts
@@ -0,0 +1,60 @@
+export const riddles = [
+  {
+    question:
+      "Tienes tres interruptores fuera de una habitación y tres bombillas dentro. Cada interruptor corresponde a una bombilla. Puedes operar los interruptores todo el tiempo que quieras, pero solo puedes entrar una vez a la habitación para inspeccionar las bombillas. ¿Cómo identificas qué interruptor acciona cada bombilla?",
+    answer:
+      "Enciendes el interruptor A durante varios minutos y lo apagas. Después enciende el interruptor B y entra en la habitacion. La bombilla caliente pero apagada será la A, la bombilla encendida corresponde a B, la bombilla fria y apagada corresponde a la C",
+    hint: "Piensa en más de una propiedad de las bombillas, no solo en encendida y apagada.",
+    keywords: ["interruptores", "bombillas", "caliente", "fria"],
+    gift: "JFRVIVH",
+  },
+  {
+    question:
+      "Estás en una encrucijada con 2 caminos. Uno lleva al tesoro, otro a la trampa. Hay 2 guardianes: uno siempre miente (Mentiroso), el otro siempre dice la verdad (Verdadoso). No sabes cuál es cuál. Solo puedes hacer una ÚNICA pregunta a SOLO UNO de ellos. ¿Qué preguntarías para saber el camino correcto?",
+    answer:
+      "Pregunta a cualquiera de los 2: ¿Si le preguntara al otro guardian cuál es el camino correcto, cuál me señalaria? Mentiroso mentira sobre lo que el Verdadoso diria y Verdadoso dira la verdad sobre la mentira del Mentiroso. Por tanto, ambos te señalarán el camino incorrecto así que tomas el camino opuesto.",
+    hint: "Este es muy fácil. Aquí no hay pista que valga.",
+    keywords: ["ambos", "los dos", "camino", "incorrecto"],
+    gift: "VM GF WVO",
+  },
+  {
+    question:
+      "Tengo ciudades pero no casas, tengo montañas pero no árboles, y tengo agua pero no peces. ¿Qué soy?",
+    answer: "Un Mapa",
+    hint: "Tengo escalas.",
+    keywords: ["mapa"],
+    gift: "KZMGZOLM",
+  },
+  {
+    question:
+      "Si tienes un reloj de arena de 7 minutos, y un reloj de arena de 11 minutos, ¿cómo puedes hervir un huevo en exactamente 15 minutos?",
+    answer:
+      "Dale la vuelta a ambos relojes cuando empiece a hervir el huevo. Cuando se agote el reloj de 7 minutos, dale la vuelta para que empiece de nuevo. 4 minutos más tarde, cuando se agote el reloj de 11 minutos (llevas 11 minutos), tendrás que darle de nuevo la vuelta al de 7 minutos y esperar a que se agote de nuevo este último. Lo que hará la suma de 11 + 4 = 15 minutos. La clave reside en la diferencia de tiempo entre ambos relojes, 4 minutos.",
+    hint: "Piensa en usar y girar ambos relojes a la vez y hacer alguna que otra operación matemática simple.",
+    keywords: [
+      "relojes",
+      "4 minutos",
+      "7 minutos",
+      "11 minutos",
+      "15 minutos",
+      "darle la vuelta a los dos",
+    ],
+    gift: "NRIZI",
+  },
+  {
+    question:
+      "¿Qué número sigue esta secuencia: 1, 11, 21, 1211, 111221, 312211, _______?",
+    answer: "13112221",
+    hint: "Lee en voz alta lo que ves.",
+    keywords: ["13112221"],
+    gift: "YLOHROOL",
+  },
+  {
+    question:
+      "Para Solucionar Hoy esTe último acertijo y EncontrAr cómo descifRar las paLaBrAs deBerás ordenAr las leTras que te eStoy dAndo y poder deCir al fin, Haber resuelto el enigmA dEl Hdp de tu novio.",
+    answer: "Search Atbash Alphabet",
+    hint: "Se una maestra estricta de lengua y luego, ordena. 15 - 1 - 6 - 8 - 2 - 3 - 4 - 14 - 9 - 7 - 18 - 10 - 20 - 11 - 13 - 5 - 12 - 17 - 19 - 16.",
+    keywords: ["atbash"],
+    gift: "Search Atbash Alphabet in Google Beby",
+  },
+];
